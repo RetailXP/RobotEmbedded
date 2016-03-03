8ea57bdd14df873a3391daa029a35d7f5328fb21
ChoiJeongwon <jw8choi@uwaterloo.ca>
2016-03-03 00:45:48 -0500

    2 way communication in Communication2

--- Communication/Communication2/Communication2.ino
+++ Communication/Communication2/Communication2.ino
@@ -1,10 +1,21 @@
-void setup(){
-  Serial.begin(9600);
+void setup()
+{
+    Serial.begin(9600);
+    Serial.setTimeout(3000);
 }
 
-void loop(){
-  Serial.println("Hello, World!");
-  delay(1000);
-  byte message = Serial.read();
-  Serial.println()
+void loop()
+{
+    Serial.println("Message1");
+//    Serial.flush();
+//    delay(1000);
+
+//    while(Serial.available())
+//    {
+        String msg = Serial.readString();
+        Serial.println(msg);
+//        Serial.flush();
+//    }
+
+//    delay(1000);
 }
